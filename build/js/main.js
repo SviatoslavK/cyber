627159bf1ce39819800b755472adccb9ba657c95
SviatoslavK <kolodaslav@gmail.com>
2016-12-08 11:32:43 +0200

    accordion fix

--- build/js/main.js
+++ build/js/main.js
@@ -7,47 +7,61 @@
 /*
  * Custom
  */
-$(function() {
-    
+$(function () {
+
     // Scroll to anchor
-    $(".navbar__list").on("click", "a", function (event) {
-          event.preventDefault();
-          var id = $(this).attr('href'),
-               top = $(id).offset().top;
-          $('body,html').animate({
-               scrollTop: top
-          }, 1000);
-     });
+    $(".navbar__list").on("click", "a", function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
+    });
 
     // Modal request
-    $('.navbar__button--text').on('click', function(e) {
+    $('.navbar__button--text').on('click', function (e) {
         e.preventDefault();
-        $('.modal__request').css({'display':'block'})
-         $('.overlay').css({'display':'block'})
+        $('.modal__request').css({
+            'display': 'block'
+        });
+        $('.overlay').css({
+            'display': 'block'
+        });
     });
-    $('#modal-req-btn').on('click', function(e) {
+    $('#modal-req-btn').on('click', function (e) {
         e.preventDefault();
-        $('.modal__request').css({'display':'none'})
-        $('.modal__thank').css({'display':'block'})
-        //  $('.overlay').css({'display':'block'})
+        $('.modal__request').css({
+            'display': 'none'
+        });
+        $('.modal__thank').css({
+                'display': 'block'
+            });
+            //  $('.overlay').css({'display':'block'})
     });
-    $('.overlay').on('click', function(e) {
+    $('.overlay').on('click', function (e) {
         e.preventDefault();
-        $('.modal__request').css({'display':'none'})
-        $('.modal__thank').css({'display':'none'})
-        $('.overlay').css({'display':'none'})
+        $('.modal__request').css({
+            'display': 'none'
+        });
+        $('.modal__thank').css({
+            'display': 'none'
+        });
+        $('.overlay').css({
+            'display': 'none'
+        });
     });
 
 
     // Tabs Strategy 
-    $('.strategy__link').on('click', function(e) {
+    $('.strategy__link').on('click', function (e) {
         e.preventDefault();
-        
+
         var item = $(this).closest('.strategy__item'),
-            content      = $('.strategy__text--inner'),
+            content = $('.strategy__text--inner'),
             itemPosition = item.index(),
-            video        = $('.strategy__board'),
-            videoNumber  = video.index();
+            video = $('.strategy__board'),
+            videoNumber = video.index();
 
         content.eq(itemPosition)
             .addClass('active__text')
@@ -57,41 +71,47 @@ $(function() {
         item.addClass('active__tab')
             .siblings()
             .removeClass('active__tab');
-        
+
         video.eq(itemPosition)
             .addClass('active__video')
             .siblings()
             .removeClass('active__video');
 
     });
-    
+
     // Accordion 
-    $('.strategy__accordion').add('.strategy__arrow').on('click', function(e) {
-       e.preventDefault();
-        
-        var $this        = $(this),
-            item         = $this.closest('.strategy__item'),
-            list         = $this.closest('.strategy__list--small'),
-            items        = list.find('.strategy__item'),
-            arrow        = item.find('.strategy__arrow'),
-            otherArrow   = list.find('.strategy__arrow'),
-            content      = item.find('.accordion__inner'),
+    $('.strategy__accordion').add('.strategy__arrow').on('click', function (e) {
+        e.preventDefault();
+
+        var $this = $(this),
+            item = $this.closest('.strategy__item'),
+            list = $this.closest('.strategy__list--small'),
+            items = list.find('.strategy__item'),
+            arrow = item.find('.strategy__arrow'),
+            otherArrow = list.find('.strategy__arrow'),
+            content = item.find('.accordion__inner'),
             otherContent = list.find('.accordion__inner'),
-            duration     = 300;
-        
+            duration = 300;
+
         if (!item.hasClass('active__accordion')) {
             items.removeClass('active__accordion');
             item.addClass('active__accordion');
-            otherContent.stop(true,true).slideUp(duration);
-            content.stop(true,true).slideDown(duration);
-            otherArrow.css({'transform': 'rotate(0deg)'})
-            arrow.css({'transform': 'rotate(90deg)'})
+            otherContent.stop(true, true).slideUp(duration);
+            content.stop(true, true).slideDown(duration);
+            otherArrow.css({
+                'transform': 'rotate(0deg)'
+            });
+            arrow.css({
+                'transform': 'rotate(90deg)'
+            });
         } else {
-            content.stop(true,true).slideUp(duration);
+            content.stop(true, true).slideUp(duration);
             item.removeClass('active__accordion');
-            arrow.css({'transform': 'rotate(0deg)'})
-        } 
-        
+            arrow.css({
+                'transform': 'rotate(0deg)'
+            });
+        }
+
     });
 
 });
